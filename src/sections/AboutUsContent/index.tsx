c6e644894f1d05ef1882def64b1bc8d013ef0bcb
wangzuo <qingjiu17@icloud.com>
2021-09-24 10:56:12 +0800

    feat: 更新版本

--- src/sections/AboutUsContent/index.tsx
+++ src/sections/AboutUsContent/index.tsx
@@ -29,23 +29,21 @@ const AboutUsContent: FC<Props> = (props) => {
           <div className='about-us-content__description'>
             <div
               className='about-us-content__description__background'
-              data-aos='fade-in'
+              data-aos='fade-right'
               data-aos-delay='500'
             ></div>
-            <span
-              className='about-us-content__description__title'
+            <div
+              className='about-us-content__description__title__wrapper'
               data-aos='fade-right'
             >
-              甘邻科技
-            </span>
-            <span
-              className='about-us-content__description__text'
-              data-aos='fade-right'
-              data-aos-delay='300'
-            >
-              是一家致力于社区物业数字化，和在线生活服务的创新型企业。
-              我们为中小型物业公司提供终身免费的数字物业平台、居民APP，和极低成本的智能硬件。通过软件、硬件、互联网的完美融合，为物业客户增加多种经营收入，提高经营水平，提升居民服务体验。让社区生活更美好
-            </span>
+              <span className='about-us-content__description__title'>
+                甘邻科技
+              </span>
+
+              <span className='about-us-content__description__title__subtitle'>
+                是一家致力于社区物业数字化，和在线生活服务的创新型企业。我们为中小型物业公司提供终身免费的数字物业平台、居民APP，和极低成本的智能硬件。通过软件、硬件、互联网的完美融合，为物业客户增加多种经营收入，提高经营水平，提升居民服务体验。让社区生活更美好
+              </span>
+            </div>
           </div>
           <div className='about-us-content__img'>
             <div className='about-us-content__img__background'></div>
